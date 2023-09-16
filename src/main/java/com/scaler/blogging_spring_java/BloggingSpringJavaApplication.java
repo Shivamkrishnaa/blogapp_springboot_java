f1467ad64f99ae530b7446aa06c88a88e3225189
SHIVAM KRISHNA <30428839+Shivamkrishnaa@users.noreply.github.com>
2023-09-16 14:16:30 +0530

    Initial commit with readme

---
+++ src/main/java/com/scaler/blogging_spring_java/BloggingSpringJavaApplication.java
@@ -0,0 +1,13 @@
+package com.scaler.blogging_spring_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloggingSpringJavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BloggingSpringJavaApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/scaler/blogging_spring_java/BloggingSpringJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.scaler.blogging_spring_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloggingSpringJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
